89af5552cd768244347d8708c142b50161dcf065
Shunsuke Uchiyama <Uchiyama.Shunsuke@cti.co.jp>
2018-06-20 13:45:11 +0900

    add overview page

--- src/pages/overview/overview.ts
+++ src/pages/overview/overview.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the OverviewPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-overview',
   templateUrl: 'overview.html',
@@ -18,7 +11,6 @@ export class OverviewPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OverviewPage');
   }
 
 }
